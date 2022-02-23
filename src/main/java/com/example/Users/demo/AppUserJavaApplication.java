a2b8a5dcddc2448c3ab0e22fb165d1ec69f9743b
LeoMG <leo01morales@gmail.com>
2022-02-23 12:51:57 -0600

    Second commit from IDE

--- src/main/java/com/example/Users/demo/AppUserJavaApplication.java
+++ src/main/java/com/example/Users/demo/AppUserJavaApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppUserJavaApplication {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(AppUserJavaApplication.class, args);
 	}
 
