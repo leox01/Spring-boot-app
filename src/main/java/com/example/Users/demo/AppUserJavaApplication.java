d3f719beb81a37dcb43e8d3c446b61cebc0a94e2
LeoMG <leo01morales@gmail.com>
2022-02-23 12:42:48 -0600

    first commit from IDE

---
+++ src/main/java/com/example/Users/demo/AppUserJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Users.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppUserJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppUserJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Users/demo/AppUserJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Users.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppUserJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
